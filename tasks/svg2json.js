5675dd52bd49b09086a7a9457d722cdb2ab4c9e7
dhaakon <dhaakon@gmail.com>
2014-04-09 17:00:04 -0700

    Changed tab spaces

--- tasks/svg2json.js
+++ tasks/svg2json.js
@@ -8,32 +8,33 @@
 
 'use strict';
 
-var		exec	=		require('child_process').exec;
-var   path  =   require('path');
-var		comm	=		path.join(__dirname , '../bin/svg2gfx.xslt');
+var     exec	=	require('child_process').exec;
+var     path    =       require('path');
+var	comm	=	path.join(__dirname , '../bin/svg2gfx.xslt');
 
 
 module.exports = function(grunt) {
   var that = this;
 
   this.parseSVG = function(context) {
-    var convert		=		function(fp){
-      var fpR         =   fp.split('/');
-      var filename    =   fpR[fpR.length - 1].split('.svg')[0];
-      var filedest    =   path.join(__dirname, '../tmp/', filename + '.json');
-      var fileorigin  =   fp;//path.join('../../' , fp);
-      var arr		  		=		['xsltproc', comm, fileorigin, '>', filedest].join(' ');
+    var convert         = function(fp){
+      var fpR           = fp.split('/');
+      var filename      = fpR[fpR.length - 1].split('.svg')[0];
+      var filedest      = path.join(__dirname, '../tmp/', filename + '.json');
+      var fileorigin    = fp;
+      var arr           = ['xsltproc', comm, fileorigin, '>', filedest].join(' ');
 
-      var fn				=		function(err, stdout, stderr){
+      var fn	        = function(err, stdout, stderr){
         if (err) console.log(err);
 
         context.done()
       }
+
       exec(arr, fn);
     }
 
-    var filterFunction    =   function (filepath)   {
-      var hasFile  =   grunt.file.exists
+    var filterFunction  = function (filepath)   {
+      var hasFile       = grunt.file.exists
 
       if(!hasFile(filepath)){
         grunt.log.warn('Source file"' + filepath + '" not found.');
@@ -43,8 +44,8 @@ module.exports = function(grunt) {
       }
     }
 
-    var fileIterator  =   function (f) {
-      var src   =   f.src.filter(filterFunction).map(convert);
+    var fileIterator    = function (f) {
+      var src           = f.src.filter(filterFunction).map(convert);
     }
 
     context.files.forEach(fileIterator)
@@ -52,7 +53,7 @@ module.exports = function(grunt) {
 
   grunt.registerMultiTask('svg2json', 'svg to json', function(){
     var options = this.options({})
-    this.done    = this.async()
+    this.done   = this.async()
 
     that.parseSVG(this);
   });
