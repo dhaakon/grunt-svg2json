3ed61de25fdcb638753308174cb90e29da6eb6e9
dhaakon <dhaakon@gmail.com>
2014-05-22 16:01:48 -0400

    Setting up multiple files

--- tasks/svg2json.js
+++ tasks/svg2json.js
@@ -13,48 +13,61 @@ var     exec	= require('child_process').exec,
         comm	= path.join(__dirname , '../bin/svg2gfx.xslt');
 
 module.exports = function(grunt) {
-  var that = this;
+	// self reference
+  var self = this;
+
+	/*
+	 *
+	 * parseSVG - function to parse the SVG and call the xsltproc
+	 * @param - context - the context to refer to
+	 *
+	 */
 
   this.parseSVG = function(context) {
-    var convert         = function(fp){
-
-      var fpR             = fp.split('/'),
-          filename        = fpR[fpR.length - 1].split('.svg')[0],
-          filedest        = path.join(__dirname, '../tmp/', filename + '.json'),
-          fileorigin      = fp,
-          arr             = ['xsltproc', comm, fileorigin, '>', filedest].join(' '),
-          fn	          = function(err, stdout, stderr){
+    var convert         = function(src, dest){
+      var fpR             = src.split('/'),
+          filedest        = context.files[0].dest,
+          fileorigin      = src,
+          arr							= ['xsltproc', comm, fileorigin, '>', filedest].join(' '),
+          fn							= function(err, stdout, stderr){
             if (err) console.log(err);
 
-            context.done()
-          }
+            context.done();
+          };
 
       exec(arr, fn);
     }
 
+			// check to see if the file exists
     var filterFunction  = function (filepath)   {
-      var hasFile       = grunt.file.exists
+      var hasFile       = grunt.file.exists;
 
       if(!hasFile(filepath)){
         grunt.log.warn('Source file"' + filepath + '" not found.');
-        return false
+        return false;
       }else{
-        return true
+        return true;
       }
     }
 
     var fileIterator    = function (f) {
-      var src           = f.src.filter(filterFunction).map(convert);
+			// grab the src then filter and convert
+			var isMoreThanOneFile = f.src.length > 1;
+
+      if (!isMoreThanOneFile) f.src.filter(filterFunction).map(convert);
     }
 
-    context.files.forEach(fileIterator)
+		// iterate through all the files
+    context.files.forEach(fileIterator);
   }
 
   grunt.registerMultiTask('svg2json', 'svg to json', function(){
-    var options = this.options({})
-    this.done   = this.async()
+		console.log(this.files[0].src);
+    var options = this.options({});
 
-    that.parseSVG(this);
+    this.done   = this.async();
+
+    self.parseSVG(this);
   });
 };
 
